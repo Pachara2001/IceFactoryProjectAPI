064cd32eda135acc785eb8d9a43ef5c2bded414a
Pachara2001 <82090806+Pachara2001@users.noreply.github.com>
2021-09-09 15:09:38 +0700

    start create API

---
+++ IceFactoryProjectAPI/src/main/java/IceFactory/IceFactory/IceFactoryApplication.java
@@ -0,0 +1,13 @@
+package IceFactory.IceFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceFactoryApplication.class, args);
+	}
+
+}
---
+++ IceFactoryProjectAPI/src/test/java/IceFactory/IceFactory/IceFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package IceFactory.IceFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
